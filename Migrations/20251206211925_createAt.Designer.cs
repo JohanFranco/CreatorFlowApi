6be39e6d76227421ed149ca2e3e84bc515be2002
Jojo Dev <johanismael350@gmail.com>
2025-12-06 15:21:36 -0600

    Auth finished

--- Migrations/20251206211925_createAt.Designer.cs
+++ Migrations/20251206211925_createAt.Designer.cs
@@ -4,6 +4,7 @@ using CreatorFlowApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CreatorFlowApi.Migrations
 {
     [DbContext(typeof(CreatorFlowDbContext))]
-    partial class CreatorFlowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251206211925_createAt")]
+    partial class createAt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
