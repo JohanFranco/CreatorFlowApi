42417ba061d467728481c9f437c2900a13b7cdab
Jojo Dev <johanismael350@gmail.com>
2025-12-07 15:21:03 -0600

    User Controller/me

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using CreatorFlowApi.DTOs.Auth;
+using CreatorFlowApi.Entities;
+using CreatorFlowApi.Repositories.Users;
+using CreatorFlowApi.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreatorFlowApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("me")]
+        public async Task<ActionResult<UserDto>> GetMe()
+        {
+            var userId = User.GetUserId();
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            var dto = _mapper.Map<UserDto>(user);
+            return Ok(dto);
+        }
+    }
+}
